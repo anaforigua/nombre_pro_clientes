d5df7ecaf3ad186077b1962acfdfcce56a304fb6
ana <anamariaforigua20@gmail.com>
2026-06-19 15:54:39 -0500

    actualizar la carpeta modelos

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
+listar_clientes: list[cliente]=[]
+
 @app.get("/")
 def inicio():
     return {"mensaje": "Este es el proyecto de clientes a desarrollar"}
@@ -10,3 +13,45 @@ def inicio():
 def  clientes():
     clientes= ["Ana", "Edward", "Johanna", "Shakira","Maluma"]
     return {"mensaje":clientes }
+
+@app.get("/clientes")
+def listar_clientes():
+    return clientes
+
+@app.get("/clientes/{id}")
+def obtener_cliente(id: int):
+    for cliente in clientes:
+        if cliente.id == id:
+            return cliente
+    raise HTTPException(404, "Cliente no encontrado")
+
+@app.post("/clientes")
+def crear_cliente(cliente: Cliente):
+    clientes.append(cliente)
+    return {
+        "mensaje": "Cliente creado",
+        "cliente": cliente
+    }
+
+@app.put("/clientes/{id}")
+def actualizar_cliente(id: int, datos: Cliente):
+
+    for i, cliente in enumerate(clientes):
+        if cliente.id == id:
+            clientes[i] = datos
+            return {
+                "mensaje": "Cliente actualizado",
+                "cliente": datos
+            }
+
+    raise HTTPException(404, "Cliente no encontrado")
+
+@app.delete("/clientes/{id}")
+def eliminar_cliente(id: int):
+
+    for cliente in clientes:
+        if cliente.id == id:
+            clientes.remove(cliente)
+            return {"mensaje": "Cliente eliminado"}
+
+    raise HTTPException(404, "Cliente no encontrado")
--- modelos/transacciones.py
+++ modelos/transacciones.py
@@ -1,53 +1,5 @@
-
-
-
-
-# CRUD TRANSACCIONES
-
-
-@app.get("/transacciones")
-def listar_transacciones():
-    return transacciones
-
-@app.post("/transacciones")
-def crear_transaccion(transaccion: Transaccion):
-
-    transacciones.append(transaccion)
-
-    return {
-        "mensaje": "Transacción creada",
-        "transaccion": transaccion
-    }
-
-@app.get("/transacciones/{id}")
-def obtener_transaccion(id: int):
-
-    for transaccion in transacciones:
-        if transaccion.id == id:
-            return transaccion
-
-    raise HTTPException(404, "Transacción no encontrada")
-
-@app.put("/transacciones/{id}")
-def actualizar_transaccion(id: int, datos: Transaccion):
-
-    for i, transaccion in enumerate(transacciones):
-        if transaccion.id == id:
-            transacciones[i] = datos
-            return {
-                "mensaje": "Transacción actualizada",
-                "transaccion": datos
-            }
-
-    raise HTTPException(404, "Transacción no encontrada")
-
-@app.delete("/transacciones/{id}")
-def eliminar_transaccion(id: int):
-
-    for transaccion in transacciones:
-        if transaccion.id == id:
-            transacciones.remove(transaccion)
-            return {"mensaje": "Transacción eliminada"}
-
-    raise HTTPException(404, "Transacción no encontrada")
-
+from pydantic import BaseModel
+class Transaccion(BaseModel):
+    id: int
+    metodo_pago: str
+    monto: float  
