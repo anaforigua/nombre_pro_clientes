c5fe242a756590ac7aa1aca0bbbf999f7b9016e4
anaforigua <anamariaforigua20@gmail.com>
2026-07-03 09:17:35 -0500

    Delete modelos directory coreccion se eliman

--- modelos/clientes.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-class Cliente(BaseModel):
-    id_cliente: int
-    nombre: str
-    correo: str
-    descripcion: str
-clientes = []
--- modelos/facturas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from modelos.clientes import Cliente
-class Factura(BaseModel):
-    id_factura: int
-    fecha: str
-    total: float
-    cliente_id:Cliente
-facturas = [] 
-
